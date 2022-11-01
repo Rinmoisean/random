f05020fc6ae87c9f24e12829a062f2e0e82323f5
Arina <arina.moana12@gmail.com>
2022-11-01 12:53:19 +0000

    40.1

---
+++ conditions/40.1.php
@@ -0,0 +1,21 @@
+<?php
+	$num = '3';
+	
+	switch ($num) {
+		case '1':
+			echo 'зима';
+		break;
+		case '2':
+			echo 'весна';
+		break;
+		case '3':
+			echo 'лето';
+		break;
+        case '4':
+			echo 'осень';
+		break;
+		default:
+			echo 'не коректный ввод';
+		break;
+	}
+?>
