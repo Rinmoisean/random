918b2d5521fccc1148ffedd9f687db7303bcdd54
Arina <arina.moana12@gmail.com>
2022-11-01 12:29:01 +0000

    39.1

---
+++ conditions/39.1.php
@@ -0,0 +1,21 @@
+<?php
+	$month = 1;
+	
+	if ($month >= 1 and $month
+		<= 3) { 
+		echo 'Зима';
+	}
+	if ($month >= 4 and $month
+		<= 6) { 
+		echo 'Весна';
+	}
+    if ($month >= 7 and $month
+		<= 9) { 
+		echo 'Лето';
+	}
+    if ($month >= 10 and $month
+		<= 12) { 
+		echo 'Осень';
+	}
+	
+?>
