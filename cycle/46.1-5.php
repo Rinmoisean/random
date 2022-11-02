e05fa0e208dc09878e150458bfdcac7616b1d322
Arina <arina.moana12@gmail.com>
2022-11-02 04:54:42 +0000

    46.1-5

---
+++ cycle/46.1-5.php
@@ -0,0 +1,26 @@
+<?php
+	
+	for ($i = 1; $i <= 100; $i++) {
+		echo $i.'  ';
+	}
+    echo '<br>';
+    echo '<br>';
+    for ($i = 11; $i <= 33; $i++) {
+		echo $i.'  ';
+	}
+    echo '<br>';
+    echo '<br>';
+    for ($i = 0; $i <= 100; $i=$i+2) {
+		echo $i.'  ';
+	}
+    echo '<br>';
+    echo '<br>';
+    for ($i = 1; $i <= 99; $i=$i+2) {
+		echo $i.'  ';
+	}
+    echo '<br>';
+    echo '<br>';
+    for ($i = 100; $i >= 0; $i--) {
+		echo $i.'  ';
+	}
+?>
