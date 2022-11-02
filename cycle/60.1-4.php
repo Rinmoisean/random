c08438b532dbdb4d0ab99c2660d649bfee9b0cb8
Arina <arina.moana12@gmail.com>
2022-11-02 16:37:26 +0000

    60.1-4

---
+++ cycle/60.1-4.php
@@ -0,0 +1,41 @@
+<?php
+	$arr = [];
+	
+	$arr[] = 1;
+	$arr[] = 2;
+	$arr[] = 3;
+	$arr[] = 4;
+	$arr[] = 5;
+	$arr[] = 6;
+        $arr[] = 7;
+        $arr[] = 8;
+        $arr[] = 9;
+        $arr[] = 10;
+
+	$a = [];
+	$a[] = 'x';
+	$a[] = 'x';
+$a[] = 'x';
+$a[] = 'x';
+$a[] = 'x';
+
+	$array = [];
+
+	for ($i = 0; $i <= 99; $i++) {
+		$array[] = $i + 1;
+	}
+
+	var_dump($array);
+	echo '<br>';
+echo '<br>';
+
+ $ar = [];
+
+        for ($i = 0; $i <= 98; $i+=2) {
+                $ar[] = $i + 1;
+        }
+
+        var_dump($ar);
+
+
+?>
