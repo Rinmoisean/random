ad248d7669659aeb6460eb64d7a295b5a2e62eca
Arina <arina.moana12@gmail.com>
2022-11-30 09:29:33 +0000

    75.1-54

--- standart_f/75.1-54.php
+++ standart_f/75.1-54.php
@@ -43,4 +43,165 @@ echo substr($str, 0, 5).'...';
 }
 else
 echo $str;
+echo '<br>';
+echo '<br>';
+//16-18
+$str='31.12.2013';
+echo str_replace('.', '-', $str);
+echo '<br>';
+echo '<br>';
+
+echo str_replace(['a', 'b', 'c'], [1, 2, 3], 'abcabc');
+echo '<br>';
+echo '<br>';
+
+$str='1a2b3c4b5d6e7f8g9h0';
+echo str_replace([1,2,3,4,5,6,7,8,9,0], '', $str);
+echo '<br>';
+echo '<br>';
+
+//19
+$str='abcabc';
+echo strtr($str, ['a'=>'1', 'b'=>'2', 'c'=>'3']);
+echo '<br>';
+echo strtr($str,'abc','123' );
+echo '<br>';
+echo '<br>';
+//20
+$str='abcde';
+echo substr_replace($str, '!!!', 2, 5);
+echo '<br>';
+echo '<br>';
+//21-26
+echo strpos('abc abc abc', 'b');
+echo '<br>';
+echo '<br>';
+echo strrpos('abc abc abc', 'b');
+echo '<br>';
+echo '<br>';
+echo strpos('abc abc abc', 'b',3);
+echo '<br>';
+echo '<br>';
+echo strpos('aaa aaa aaa aaa aaa', ' ', 4);
+echo '<br>';
+echo '<br>';
+if(strrpos('http://code.mu/','http://')===0) 
+	{echo '+';}
+	else echo '-';
+	echo '<br>';
+echo '<br>';
+//27-29
+$date = 'html css php';
+	$arr = explode(' ', $date);
+	var_dump($arr);
+echo '<br>';
+echo '<br>';
+$arr = ['html', 'css', 'php'];
+echo implode(',', $arr);
+echo '<br>';
+echo '<br>';
+$date = '2022-11-18';
+	$arr = explode('-', $date);
+	echo $arr[2].'.'.$arr[1].'.'.$arr[0];
+	echo '<br>';
+echo '<br>';
+//30-32
+$str = '1234567890';
+	
+$arr = str_split($str, 2);
+var_dump($arr);
+echo '<br>';
+echo '<br>';
+$arr = str_split($str, 1);
+var_dump($arr);
+echo '<br>';
+echo '<br>';
+$arr = str_split($str, 2);
+echo implode('-', $arr);
+echo '<br>';
+echo '<br>';
+//33-35
+$str = ' hfhjmg hhzaf ';
+echo trim($str);
+echo '<br>';
+echo '<br>';
+$str = '/php/';
+echo trim($str, '/');
+echo '<br>';
+echo '<br>';
+$str = 'слова слова слова.';
+echo rtrim($str, '.').'.';
+echo '<br>';
+echo '<br>';
+//36-37
+$str = '12345';
+echo strrev($str);
+echo '<br>';
+echo '<br>';
+$str = 'level';
+$eStr = ($str == strrev($str)) ? 'является' : 'не является';
+echo $eStr;
+echo '<br>';
+echo '<br>';
+//38-39
+echo str_shuffle($str);
+echo '<br>';
+echo '<br>';
+$str = 'abcdefghiklmnopqrstvxyz';
+$s = str_shuffle($str);
+echo substr($s, 0, 6);
+echo '<br>';
+echo '<br>';
+//40
+$str = '12345678';
+echo number_format($str, 0, ',', ' ');
+echo '<br>';
+echo '<br>';
+//42
+for ($i = 1; $i<10; $i++){
+echo str_repeat($i, $i).'<br>';
+}
+echo '<br>';
+echo '<br>';
+//43-44
+$str = 'html, <b>php</b>, js';
+echo strip_tags($str);
+echo '<br>';
+echo '<br>';
+echo strip_tags($str, '<b><i>');
+echo '<br>';
+echo '<br>';
+//46-50
+echo ord('a').'<br>';
+echo ord('b').'<br>';
+echo ord('c').'<br>';
+echo ord(' ');
+echo '<br>';
+echo '<br>';
+echo chr(33);
+echo '<br>';
+echo '<br>';
+$str = chr(rand(65,90));
+echo $str;
+echo '<br>';
+echo '<br>';
+$str = '';
+$len = 7;
+for ($i = 0; $i < $len; $i++)
+	$str .= chr(rand(97,122));
+echo $str;
+echo '<br>';
+echo '<br>';
+//52-53
+$str = 'ab-cd-ef';
+echo strchr($str, '-');
+echo '<br>';
+echo '<br>';
+echo strrchr($str, '-');
+echo '<br>';
+echo '<br>';
+$str = 'ab--cd--ef';
+echo strstr($str, '--');
+echo '<br>';
+echo '<br>';
 ?>
