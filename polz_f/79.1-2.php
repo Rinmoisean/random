71fb6ffbdf591cd59c1e1401189f153d4858cbf9
Arina <arina.moana12@gmail.com>
2022-12-04 09:47:12 +0000

    polz

---
+++ polz_f/79.1-2.php
@@ -0,0 +1,19 @@
+<?php
+//1
+	func(); 
+	
+	function func() {
+		echo 'Arina';
+		echo '<br>';
+		echo '<br>';
+	}
+//2
+function fu() {
+	$sum=0;
+	for($i=1; $i<=100; $i++){
+	$sum+=$i;}
+	echo($sum);
+}
+
+fu();
+?>
---
+++ polz_f/82.1-3.php
@@ -0,0 +1,37 @@
+<?php
+//1
+	function getSquareSum($arr) {
+		$sum = 0;
+		
+		foreach ($arr as $elem) {
+			$sum += pow( $elem);
+		}
+		
+		return $sum;
+	}
+
+	func();
+//2
+
+function getDivisors($num) {
+	$i=0;
+	$result = [];
+	foreach ($num as $i) {
+		$i++;
+if($num % $i == 0)
+array_push($result, $i);
+return $result;
+}
+}
+func();
+//3
+function delElem($arr, $num){
+	$arr=[];
+	foreach($arr as $elem){
+		if ($arr[] === $num)
+echo $arr[];
+		
+	}
+}
+func();
+?>
